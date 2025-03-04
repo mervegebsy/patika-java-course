d26109638a4902ed31ec0af222d695b34acef74d
mervegebsy <mervegebespy@gmail.com>
2025-03-04 16:38:26 +0300

    vücut kitle indeksi düzenlendi

--- src/main/java/week1/BodyMassIndex.java
+++ src/main/java/week1/BodyMassIndex.java
@@ -9,6 +9,7 @@ public class BodyMassIndex {
         double height = scanner.nextDouble();
         System.out.println("Lütfen kilonuzu giriniz: ");
         double kg = scanner.nextDouble();
+
         double bmi = kg / (height * height); // vücut kitle indeksi formülü kg / boyun karesi
         System.out.println("Vücut kitle indeksiniz : " + bmi);
     }

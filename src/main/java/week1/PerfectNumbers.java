c710304e477edf7039844f8e67b37b6ac250b7e1
mervegebsy <mervegebespy@gmail.com>
2025-03-09 16:04:45 +0300

    yorum satırları eklendi

--- src/main/java/week1/PerfectNumbers.java
+++ src/main/java/week1/PerfectNumbers.java
@@ -6,14 +6,14 @@ public class PerfectNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bir sayı giriniz: ");
-        int number = scanner.nextInt();
+        int number = scanner.nextInt(); // kullanıcıdan aldığımız değeri numberda tutuyoruz
         int sum = 0;
         for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
+            if (number % i == 0) { // numberın bölenlerini bulup topluyoruz
                 sum = sum + i;
             }
         }
-        if (sum == number){
+        if (sum == number){ // toplam sayıya eşit mi kontrolü yapıyoruz
             System.out.print(number + " bir mükemmel sayıdır");
         }
         else {

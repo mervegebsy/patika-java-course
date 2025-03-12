c4a29c58ecd8d5e8f10c78492781e98063d6e817
mervegebsy <mervegebespy@gmail.com>
2025-03-12 20:33:11 +0300

    Şifre kontrolü taskı tamamlandı

---
+++ src/main/java/week2/PasswordControl.java
@@ -0,0 +1,17 @@
+package week2;
+
+import java.util.Scanner;
+
+public class PasswordControl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir şifre giriniz: ");
+        String password = scanner.next();
+        if(password.length() >= 8 && !password.contains(" ")  && Character.isUpperCase(password.charAt(0)) && password.endsWith("?")){
+            System.out.println("Geçerli şifre");
+        }
+        else {
+            System.out.println("Şifre geçersizdir");
+        }
+    }
+}

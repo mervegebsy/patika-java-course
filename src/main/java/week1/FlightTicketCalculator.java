224bea07621857e831961d3d1b6f2336f3b70e96
mervegebsy <mervegebespy@gmail.com>
2025-03-13 02:01:48 +0300

    uçak bileti hesaplama verilen senaryolara göre yapıldı

--- src/main/java/week1/FlightTicketCalculator.java
+++ src/main/java/week1/FlightTicketCalculator.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class FlightTicketCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Mesafeyi giriniz: ");
+        System.out.print("Mesafeyi giriniz: ");
         double distance = scanner.nextDouble();
-        System.out.println("Yaşınızı giriniz: ");
+        System.out.print("Yaşınızı giriniz: ");
         int age = scanner.nextInt();
-        System.out.println("Yolculuk tipinizi giriniz:( 1 => Tek Yön , 2 => Gidiş Dönüş) ");
+        System.out.print("Yolculuk tipinizi giriniz:( 1 => Tek Yön , 2 => Gidiş Dönüş: ) ");
         int travelType = scanner.nextInt();
+
         double price = distance * 0.10;
-        if( distance > 0) {
-            if (age < 12) {
+
+        if(distance > 0 && age > 0 && (travelType == 1 || travelType == 2)){ //kullanıcıdan istenenlerin kontrolü
+            if (age < 12) { // yaşa göre indirim tarifelerinin kontrolü
                 price = price / 2;
             } else if (age > 12 && age < 24) {
                 price = price * 0.9;
@@ -21,16 +23,14 @@ public class FlightTicketCalculator {
                 price = price * 0.7;
 
             }
+            if (travelType == 2) { // yolculuk tipine göre indirim tarifesi kontrolü
+                price = price * 0.8 *2;
+                System.out.println(price);
+            } else if (travelType == 1) {
+                System.out.println(price);
+            }
         }
-        else {
-            System.out.println("Hatalı veri girişi yaptınız");
-        }
-        if (travelType ==2) {
-            price = price * 0.8 *2;
-            System.out.println(price);
-        } else if (travelType == 1) {
-            System.out.println(price);
-        }else {
+        else { // girilen veriler yanlışsa
             System.out.println("Hatalı veri girişi yaptınız");
         }
 
